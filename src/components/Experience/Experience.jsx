4ef306e6bf3214a89bc0402645f781161af1d0f5
Monikafullstack <157715298+Monikafullstack@users.noreply.github.com>
2024-01-26 08:11:48 +0530

    Add files via upload

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Experience.module.css";
+import skills from "../../data/skills.json"; // Assuming skills.json contains an array of objects with properties like id, title, and imageSrc
+import { getImageUrl } from "../../utils";
+
+export const Experience = () => {
+  return (
+    <section className={styles.container} id="experience">
+      <h2 className={styles.title}>Skills</h2>
+      <div className={styles.content}>
+        <div className={styles.skills}>
+          {skills.map((skill) => {
+            return (
+              <div key={skill.id} className={styles.skill}>
+                <div className={styles.skillImageContainer}>
+                  <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                </div>
+                <p>{skill.title}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
